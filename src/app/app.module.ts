dc60c453e1e4422a0e767964a54e800b13c8f4bd
Cesar Acuña <cesar.acu@hotmail.com>
2022-07-19 21:39:31 -0500

    Maqueta bankapp1.jpg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { CuentasComponent } from './cuentas/cuentas.component';
+import { ContentsComponent } from './contents/contents.component';
+import { MovimientosComponent } from './movimientos/movimientos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    CuentasComponent,
+    ContentsComponent,
+    MovimientosComponent
   ],
   imports: [
     BrowserModule,
